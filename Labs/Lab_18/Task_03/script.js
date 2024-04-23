af7f4ed8b23e2d7414cee7cc395a2f13f57ebb9e
Oleksandr Holan <game.play.for.alex@gmail.com>
2024-04-23 15:19:29 +0300

    lab 18 task 3 done

--- Labs/Lab_18/Task_03/script.js
+++ Labs/Lab_18/Task_03/script.js
@@ -1,3 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const columns = document.querySelectorAll('.kanban-column');
+    const items = document.querySelectorAll('.kanban-item');
+
+    items.forEach(item => {
+        item.setAttribute('draggable', "true");
+        item.addEventListener('dragstart', drag);
+    });
+
+    columns.forEach(column => {
+        column.addEventListener('dragover', allowDrop);
+        column.addEventListener('drop', drop);
+    });
+});
+
 function allowDrop(ev) {
     ev.preventDefault();
 }
